e69a0dd27367bd950b0226801197aa2f9939ad23
David Barros <dwbarros@gmail.com>
2022-06-07 15:27:18 +0100

    Modal ViewImage.

--- src/components/Modal/ViewImage.tsx
+++ src/components/Modal/ViewImage.tsx
@@ -8,16 +8,53 @@ import {
   Link,
 } from '@chakra-ui/react';
 
+
 interface ModalViewImageProps {
   isOpen: boolean;
   onClose: () => void;
   imgUrl: string;
 }
 
+
 export function ModalViewImage({
   isOpen,
   onClose,
   imgUrl,
 }: ModalViewImageProps): JSX.Element {
-  // TODO MODAL WITH IMAGE AND EXTERNAL LINK
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent
+        m="2rem auto"
+        bg="transparent"
+        w="auto"
+        h="auto"
+        maxW="80vw"
+        maxH="80vh"
+      >
+        <ModalBody p="0">
+          <Image
+            src={imgUrl}
+            h="100%"
+            w="100%"
+          />
+        </ModalBody>
+
+        <ModalFooter 
+          h="1.5rem"
+          bg="pGray.800"
+          borderBottomRadius="6px"
+          justifyContent="left"
+        >
+          <Link
+            href={imgUrl}
+            isExternal
+            fontSize="0.875rem"
+          >
+            Abrir original
+          </Link>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
 }
